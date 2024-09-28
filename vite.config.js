1b670b0ee17729ccd7c7689ee0e65d0385da34a4
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-09-28 22:44:19 +0400

    no changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/personal-website",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["react-icons/fa"], // Add this line if you're still facing issues
+    },
+  },
 });
